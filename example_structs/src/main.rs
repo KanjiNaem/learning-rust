eb5290732a59fd80e3a1497a01d40af1f9457361
KanjiNaem <janik.jehmlich@gmail.com>
2022-11-14 10:20:14 +0100

    implement rust struct example

---
+++ example_structs/src/main.rs
@@ -0,0 +1,30 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    // tuple version
+    let rect = (30, 50);
+    // println!("The area of the rectangle is {} square pixels.", area_from_tuple(rect));
+
+    fn area_from_tuple(dimensions: (u32, u32)) -> u32 {
+        dimensions.0 * dimensions.1
+    }
+
+    // struct verison
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_from_struct(&rect1)
+    );
+
+    fn area_from_struct(rectangle: &Rectangle) -> u32 {
+        rectangle.width * rectangle.height
+    }
+}
