e2cbe8e05de010da7e420bfcafe1a194185b591d
KanjiNaem <janik.jehmlich@gmail.com>
2022-11-16 13:21:48 +0100

    blind comment

--- example_structs/src/main.rs
+++ example_structs/src/main.rs
@@ -1,30 +1,30 @@
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
 
 fn main() {
-    // tuple version
-    let rect = (30, 50);
-    // println!("The area of the rectangle is {} square pixels.", area_from_tuple(rect));
+    //     // tuple version
+    //     let rect = (30, 50);
+    //     // println!("The area of the rectangle is {} square pixels.", area_from_tuple(rect));
 
-    fn area_from_tuple(dimensions: (u32, u32)) -> u32 {
-        dimensions.0 * dimensions.1
-    }
+    //     fn area_from_tuple(dimensions: (u32, u32)) -> u32 {
+    //         dimensions.0 * dimensions.1
+    //     }
 
-    // struct verison
+    //     // struct verison
 
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
+    //     let rect1 = Rectangle {
+    //         width: 30,
+    //         height: 50,
+    //     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area_from_struct(&rect1)
-    );
+    //     println!(
+    //         "The area of the rectangle is {} square pixels.",
+    //         area_from_struct(&rect1)
+    //     );
 
-    fn area_from_struct(rectangle: &Rectangle) -> u32 {
-        rectangle.width * rectangle.height
-    }
+    //     fn area_from_struct(rectangle: &Rectangle) -> u32 {
+    //         rectangle.width * rectangle.height
+    //     }
 }
